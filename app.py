7d53ec9cb368471194b7b9840572afc9e13fc557
sammyschapps87 <sms748@scarletmail.rutgers.edu>
2021-09-14 12:30:47 -0400

    began app.py

--- app.py
+++ app.py
@@ -4,9 +4,47 @@ import pandas as pd
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
+from sqlalchemy.sql.expression import true
+from sqlalchemy.sql.schema import MetaData
 
 
-engine
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect = True)
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+            f"/api/v1.0/stations<br/>"
+            f"/api/v1.0/tobs<br/>"
+            f"api/v1.0/start<br/>"
+        f"/api/v1.0<start>/<end>"
+    )
+
+@app.route ('/api/v1.0/precipitation<br/>')
+    def percipitation():
+
+        results = session.query(Measurement.date, Measurement.prcp).all()
+        result_dict = dict(results)
+        return jsonify(result_dict)
+
+@app.route ('/api/v1.0/stations<br/>')
+    def stations():
+
+        results = session.query(Stations.stations).all()
+        results_list = list(results)
+        return jsonify(results_list)
+
+@app.route ('/api/v1.0/tobs<br/>')
+    def tobs():
+        results = session.query(Measurement.date, Stations.tempature)
+
